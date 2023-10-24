051eb38ec8a4ddbb760bed63da41a2c842181a40
haoliang <haoliang0.1.2@gmail.com>
2023-10-24 09:20:08 +0800

    sync with upstream

--- colors/boneyard.lua
+++ colors/boneyard.lua
@@ -17,147 +17,145 @@
 --
 
 local ex = require("infra.ex")
+local hi = require("infra.highlighter")(0)
 
-local api = vim.api
-
----for 'cterm=none', `{bold = nil, underline = nil}` does the same
----@param group string
----@param cterm_spec {fg: integer|string, bg: integer|string, bold?: boolean, underline?: boolean}
-local function hi(group, cterm_spec)
-  local spec = {
-    ctermfg = cterm_spec.fg,
-    ctermbg = cterm_spec.bg,
-    cterm = {
-      bold = cterm_spec.bold,
-      underline = cterm_spec.underline,
-    },
-  }
-  api.nvim_set_hl(0, group, spec)
-end
-
+-- stylua: ignore
 do --main
   do -- prelude
     assert(vim.go.background == "dark")
     ex("highlight", "clear")
-    vim.g.colors_name = "boothill"
+    vim.g.colors_name = "boneyard"
   end
 
   do --vim relevant
-    hi("Folded", { fg = 243 })
-    hi("FoldColumn", { fg = 243, bold = true })
-    hi("SignColumn", {})
-    hi("Visual", { fg = 7, bg = 2 })
-    hi("VisualNOS", { bg = 2 })
-    hi("StatusLine", { fg = 166, bold = true, underline = true })
+    hi("Folded",       { fg = 243 })
+    hi("FoldColumn",   { fg = 243, bold = true })
+    hi("SignColumn",   {})
+    hi("Visual",       { fg = 7, bg = 2 })
+    hi("VisualNOS",    { bg = 2 })
+    hi("StatusLine",   { fg = 166, bold = true, underline = true })
     hi("StatusLineNC", { bold = true, underline = true })
     hi("WinsEparator", { fg = 7 })
 
-    hi("IncSearch", { bg = 178, bold = true })
-    hi("Search", { bg = 3 })
+    hi("IncSearch",    { bg = 178, bold = true })
+    hi("Search",       { bg = 3 })
 
-    hi("WildMenu", { bg = 222 })
+    hi("WildMenu",     { bg = 222 })
 
-    hi("PMenu", { fg = 7, bg = 8 })
-    hi("PMenuSel", { fg = 7, bg = 30, bold = true })
-    hi("PMenuSbar", { fg = 8, bg = 7 })
-    hi("PMenuThumb", { fg = 7, bg = 8 })
+    hi("PMenu",        { fg = 7, bg = 8 })
+    hi("PMenuSel",     { fg = 7, bg = 30, bold = true })
+    hi("PMenuSbar",    { fg = 8, bg = 7 })
+    hi("PMenuThumb",   { fg = 7, bg = 8 })
 
     hi("CursorColumn", {})
-    hi("CursorLine", { bold = true })
+    hi("CursorLine",   { bold = true })
 
-    hi("TabLine", { fg = 248 })
-    hi("TabLineSel", { fg = 130, bold = true })
-    hi("TabLineFill", {})
+    hi("TabLine",      { fg = 248 })
+    hi("TabLineSel",   { fg = 130, bold = true })
+    hi("TabLineFill",  {})
 
-    hi("LineNr", { fg = 10 })
+    hi("LineNr",       { fg = 10 })
     hi("CursorLineNr", { bold = true })
 
     --misc
-    hi("Whitespace", { fg = 15 })
-    hi("MatchParen", { fg = 15, bg = 14 })
+    hi("Whitespace",   { fg = 15 })
+    hi("MatchParen",   { fg = 15, bg = 14 })
     hi("MsgSeparator", { fg = 9, bg = 15, underline = true })
   end
 
   do --diff
-    hi("diffAdded", { fg = 7 })
+    hi("diffAdded",   { fg = 7 })
     hi("diffRemoved", { fg = 243 })
     hi("diffChanged", { fg = 5 })
-    hi("diffFile", { fg = 0, bold = true })
-    hi("gitDiff", { fg = 0 })
+    hi("diffFile",    { fg = 250 })
+    hi("gitDiff",     { fg = 243 })
   end
 
   do --statusline
-    hi("StatusLineBufStatus", { fg = 7, bold = true })
-    hi("StatusLineFilePath", { fg = 130 })
-    hi("StatusLineAltFile", { fg = 248 })
-    hi("StatusLineCursor", { fg = 7 })
-    hi("StatusLineSpan", { fg = 15 })
-    hi("StatusLineRepeat", { fg = 7 })
+    hi("StlDirty",   { fg = 7,  bold = true })
+    hi("StlFile",    { fg = 130 })
+    hi("StlAltFile", { fg = 248 })
+    hi("StlCursor",  { fg = 7   })
+    hi("StlSpan",    { fg = 15  })
+    hi("StlRepeat",  { fg = 7   })
+    hi("StlErrors",  { fg = 1,  bold = true })
   end
 
   do --general grammar token
     --:h group-name
 
     --rest: Underlined, Ignore,   Error
-    hi("Normal", { fg = 15 })
-    hi("Comment", { fg = 244 })
-    hi("Todo", { fg = 9, bg = 15, bold = true })
+    hi("Normal",        { fg = 15 })
+    hi("Comment",       { fg = 244 })
+    hi("Todo",          { fg = 9, bold = true })
 
     --any constant
     --rest: Character Number Boolean Float
-    hi("Constant", { fg = 7 })
-    hi("String", { fg = 248 })
+    hi("Constant",      { fg = 7 })
+    hi("String",        { fg = 248 })
 
     --any variable name
-    hi("Identifier", { fg = 7 })
-    hi("Function", { fg = 7 })
+    hi("Identifier",    { fg = 7 })
+    hi("Function",      { fg = 7 })
 
     --any statement
     --rest: Operator Keyword Conditional Repeat Label Exception
-    hi("Statement", { fg = 248 })
+    hi("Statement",     { fg = 248 })
 
     --generic Preprocessor
     --rest: Include Define Macro PreCondit
-    hi("PreProc", { fg = 7 })
+    hi("PreProc",       { fg = 7 })
 
     --int,              long,     char,    etc; struct, union, enum, etc.
     --rest: Structure Typedef StorageClass
-    hi("Type", { fg = 7 })
+    hi("Type",          { fg = 7 })
 
     --any special symbol
     --rest: SpecialChar Tag SpecialComment Debug
-    hi("Special", { fg = 7 })
-    hi("Delimiter", { fg = 248 })
+    hi("Special",       { fg = 7 })
+    hi("Delimiter",     { fg = 248 })
   end
 
   do --lsp
-    hi("@error", { fg = 7, underline = true })
+    hi("@error",                 { fg = 7, underline = true })
 
-    hi("@function", { fg = 7 })
-    hi("@variable", { fg = 7 })
-    hi("@function.builtin", { fg = 7 })
-    hi("@type.builtin", { fg = 7 })
-    hi("@variable.builtin", { fg = 7 })
-    hi("@constant.builtin", { fg = 7 })
+    hi("@function",              { fg = 7 })
+    hi("@variable",              { fg = 7 })
+    hi("@function.builtin",      { fg = 7 })
+    hi("@type.builtin",          { fg = 7 })
+    hi("@variable.builtin",      { fg = 7 })
+    hi("@constant.builtin",      { fg = 7 })
 
-    hi("@keyword", { fg = 31 })
-    hi("@keyword.function", { fg = 31 })
-    hi("@function.macro", { fg = 31 })
+    hi("@keyword",               { fg = 31 })
+    hi("@keyword.function",      { fg = 31 })
+    hi("@function.macro",        { fg = 31 })
 
-    hi("@keyword.return", { fg = 166 })
+    hi("@keyword.return",        { fg = 166 })
 
-    hi("@keyword.operator", { fg = 248 })
-    hi("@exception", { fg = 248 })
-    hi("@type.qualifier", { fg = 248 })
-    hi("@string.escape", { fg = 248 })
-    hi("@string.special", { fg = 248 })
+    hi("@keyword.operator",      { fg = 248 })
+    hi("@exception",             { fg = 248 })
+    hi("@type.qualifier",        { fg = 248 })
+    hi("@string.escape",         { fg = 248 })
+    hi("@string.special",        { fg = 248 })
     hi("@punctuation.delimiter", { fg = 248 })
-    hi("@punctuation.special", { fg = 248 })
-    hi("@punctuation.bracket", { fg = 248 })
-    hi("@punctuation.bracket", { fg = 248 })
+    hi("@punctuation.special",   { fg = 248 })
+    hi("@punctuation.bracket",   { fg = 248 })
+    hi("@punctuation.bracket",   { fg = 248 })
   end
 
   do --diagnostic
     hi("DiagnosticHint", { fg = 10 })
+  end
+
+  do --git
+    hi("githash",  { fg = 31 })
+    hi("gitEmail", { fg = 250 })
+    hi("gitDate",  { fg = 250 })
+  end
+
+  do --quickfix
+    hi("qfFileName",  { fg = 244 })
+    hi("qfSeparator", { fg = 244 })
+    hi("qfLineNr",    { fg = 244 })
   end
 end
--- readme.md
+++ readme.md
@@ -3,14 +3,15 @@ an opinionated nvim colorscheme
 ![](https://user-images.githubusercontent.com/6236829/253806385-37a92aef-8b57-4e26-9311-d9743bf939c3.jpg)
 (that's how it looks in my terminal with solarized-dark theme)
 
-## features, flavors
+## features, flavors, limits
 * dark theme, it forces &background=dark
 * only for terminal with 8-bit color, it uses color code 0-255
     * which also means you should define the color palette in your terminal
+    * and you have `&termguicolors=0`
 * no much colors, it uses almost 4 colors
 * no trying to be compatible with 3rd plugins
-* no much styles
-* support treesitter highlight groups
+* no much styles (bold,underline...)
+* support builtin highlight groups: treesitter, diagnostic
 
 ## prerequisites
 * nvim 0.9.*
